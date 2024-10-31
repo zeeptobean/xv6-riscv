5e6b14e99a1e4be8a2d955275010e94dcf18570b
Andy Zhang <37402126+AnzhiZhang@users.noreply.github.com>
2024-10-31 16:09:24 +0000

    refactor: clear user.h

--- user/user.h
+++ user/user.h
@@ -29,14 +29,16 @@ char* strcpy(char*, const char*);
 void *memmove(void*, const void*, int);
 char* strchr(const char*, char c);
 int strcmp(const char*, const char*);
-void fprintf(int, const char*, ...) __attribute__ ((format (printf, 2, 3)));
-void printf(const char*, ...) __attribute__ ((format (printf, 1, 2)));
 char* gets(char*, int max);
 uint strlen(const char*);
 void* memset(void*, int, uint);
 int atoi(const char*);
 int memcmp(const void *, const void *, uint);
 void *memcpy(void *, const void *, uint);
+
+// printf.c
+void fprintf(int, const char*, ...) __attribute__ ((format (printf, 2, 3)));
+void printf(const char*, ...) __attribute__ ((format (printf, 1, 2)));
 
 // umalloc.c
 void* malloc(uint);
