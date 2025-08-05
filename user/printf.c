d234f7b274a036f51f9342df21c86e281f2fae02
Frans Kaashoek <kaashoek@mit.edu>
2025-08-05 10:13:31 -0400

    Kill #if 0

--- user/printf.c
+++ user/printf.c
@@ -108,33 +108,6 @@ vprintf(int fd, const char *fmt, va_list ap)
         putc(fd, c0);
       }
 
-#if 0
-      if(c == 'd'){
-        printint(fd, va_arg(ap, int), 10, 1);
-      } else if(c == 'l') {
-        printint(fd, va_arg(ap, uint64), 10, 0);
-      } else if(c == 'x') {
-        printint(fd, va_arg(ap, int), 16, 0);
-      } else if(c == 'p') {
-        printptr(fd, va_arg(ap, uint64));
-      } else if(c == 's'){
-        s = va_arg(ap, char*);
-        if(s == 0)
-          s = "(null)";
-        while(*s != 0){
-          putc(fd, *s);
-          s++;
-        }
-      } else if(c == 'c'){
-        putc(fd, va_arg(ap, uint));
-      } else if(c == '%'){
-        putc(fd, c);
-      } else {
-        // Unknown % sequence.  Print it to draw attention.
-        putc(fd, '%');
-        putc(fd, c);
-      }
-#endif
       state = 0;
     }
   }
