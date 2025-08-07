ab958d71b9a41ebf966c57bd7738616af5c87427
Frans Kaashoek <kaashoek@mit.edu>
2025-08-07 16:45:23 -0400

    Revert "Document that name in dirent may not be a null-terminated string."
    Not supported by clang
    
    This reverts commit 5aed199428c58adb62af680c31de9231202b6635.

--- kernel/fs.h
+++ kernel/fs.h
@@ -53,10 +53,8 @@ struct dinode {
 // Directory is a file containing a sequence of dirent structures.
 #define DIRSIZ 14
 
-// The name field may have DIRSIZ characters and not end in a NULL
-// character.
 struct dirent {
   ushort inum;
-  char name[DIRSIZ]  __attribute__((nonstring));;
+  char name[DIRSIZ];
 };
 
